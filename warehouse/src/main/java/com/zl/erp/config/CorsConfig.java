2d46ef6af01233114c2e5d2ca7cc387e69614492
chinaZTLV <qwezhutao@foxmail.com>
2019-10-26 19:08:28 +0800

    增加跨域配置

---
+++ warehouse/src/main/java/com/zl/erp/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.zl.erp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description: 跨域配置
+ * @Author: zhutao
+ * @Date: 2019/9/19
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
